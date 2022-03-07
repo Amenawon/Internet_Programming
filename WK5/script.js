0edce99a277cac1dc59a0f41626954beb84b981a
Amenawon <menaesezobor@gmail.com>
2022-03-07 17:00:48 +0000

    complete exercise 7 solution

--- WK5/script.js
+++ WK5/script.js
@@ -1,31 +1,36 @@
 function loadCircle() {
     var circleEl = document.getElementById('circle');
-    circleEl.addEventListener('click',setTimer);
-    circleEl.style.opacity=1;
+    document.addEventListener('keydown', moveElementToLeft);
+    circleEl.style.opacity = 1;
 }
-
-function displayAlert(){
+function moveElementToLeft() {
     var circleEl = document.getElementById('circle');
-    circleEl.style.opacity=0.5;
-    circleEl.style.backgroundColor='blue';
+    var positionFromLeft = circleEl.offsetLeft;
+    circleEl.style.left = (positionFromLeft - 10) + 'px';
+
+}
+function displayAlert() {
+    var circleEl = document.getElementById('circle');
+    circleEl.style.opacity = 0.5;
+    circleEl.style.backgroundColor = 'blue';
     alert('button was pressed!')
-    
+
 }
 
-function makeCircleOpaque(){
+function makeCircleOpaque() {
     var circleEl = document.getElementById('circle');
-    var circleOpacity= parseFloat(circleEl.style.opacity);
-    circleEl.style.opacity=circleOpacity + 0.1;
-       
+    var circleOpacity = parseFloat(circleEl.style.opacity);
+    circleEl.style.opacity = circleOpacity + 0.1;
+
 }
 
-function setTimer(){
-    setInterval(makeCircleTransparent,100)
+function setTimer() {
+    setInterval(makeCircleTransparent, 100)
 }
-function makeCircleTransparent(){
+function makeCircleTransparent() {
     var circleEl = document.getElementById('circle');
-    var circleOpacity= parseFloat(circleEl.style.opacity);
-    circleEl.style.opacity=circleOpacity - 0.1;
-       
+    var circleOpacity = parseFloat(circleEl.style.opacity);
+    circleEl.style.opacity = circleOpacity - 0.1;
+
 }
 document.addEventListener('DOMContentLoaded', loadCircle);
